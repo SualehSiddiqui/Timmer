01e6fbc22ad9fa7a07ab1698891eda50a5e7ba98
Sualeh Siddiqui <sualehmsiddiqui@gmail.com>
2023-05-19 16:16:32 +0500

    timmer

--- app.js
+++ app.js
@@ -1,26 +1,54 @@
-var displayTime = document.querySelector("#time").value;
-var sec = 00;
-var min = 00;
-var hour = 00;
+var displayTime = document.querySelector("#time");
+var displayTimeValue = document.querySelector("#time").value;
+var sec0;
+var sec;
+var min0;
+var min;
+var hour0;
+var hour;
 var onOrOff;
-console.log(displayTime)
+// console.log(displayTimeValue)
 
 function timmer(){
-    
+    -- sec
+console.log(sec)
+
+    if(sec == 0){
+        sec = 60;
+        --min;
+        if(min === 0){
+            min = 60;
+            --hour
+        }
+    }
+    var s = sec < 10 ? "0" + sec : sec ;
+    var m = min < 10 ? "0" + min : min ;
+    var h = hour < 10 ? "0" + hour : hour ;
+
+    displayTime.value = h + " : " + m + " : " + s;
+}
+
+function valueOfWatch(){
+    sec0 = displayTimeValue.slice(6);
+    sec = sec0 < 10 ? displayTimeValue.slice(7) : sec0;
+    console.log(sec)
+    min0 = displayTimeValue.slice(3, 5);
+    min = min0 < 10 ? displayTimeValue.slice(4, 5) : min0;
+    hour0 = displayTimeValue.slice(0, 2);
+    hour = hour0 < 10 ? displayTimeValue.slice(1, 2) : hour0;
 }
 
 function startWatch(){
-    onOrOff =   setInterval(stopWatch , 1000)
+   valueOfWatch()
+   console.log(displayTimeValue)
+    onOrOff = setInterval(timmer , 1000);
 }
 
 function pauseWatch(){
-    clearInterval(onOrOff)
+    clearInterval(onOrOff);
 }
 
 function resetWatch(){
     clearInterval(onOrOff)
-    sec = 0;
-    min = 0;
-    hour = 0;
-    displayTime.innerHTML = "00 : 00 : 00";
+    displayTime.value = "00:00:00";
 }
